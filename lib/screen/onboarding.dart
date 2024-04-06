599e8c139bf6b318bbd68641b93235be9ae166f3
Ahmed Asaad <ahmed.asad3988@gmail.com>
2024-04-06 20:00:01 +0200

    finish_ui

--- lib/screen/onboarding.dart
+++ lib/screen/onboarding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:roaia/localization/localization_methods.dart';
 import 'package:roaia/screen/login.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -26,28 +27,28 @@ class _Onborading_ScreenState extends State<Onborading_Screen> {
 
   bool isLast = false;
 
-  List<Boarding> boarding=
-  [
-    Boarding(
-      image:"assets/images/image1.png" ,
-      title:"GPS Monitoring",
-      body: "   Gps monitoring to know where the\n blind is by the place where the roaia \n                      glasses in .",
-    ),
-    Boarding(image:"assets/images/image2.png" ,
-      title:"SOS Notification",
-      body: "We provide SOS notification if he fall in trouble  there is a notification sent  directly to app and the glasses take photo then sent them to app  then escort can  go to target place without any  problem or obstacle. ",
-    ),
-    Boarding(image:"assets/images/image3.png" ,
-      title:"Add Contact",
-      body: "Add contact feature which make glasses know people front of it.",
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
+    List<Boarding> boarding=
+    [
+      Boarding(
+        image:"assets/images/image1.png" ,
+        title:tr("gps_monitoring", context),
+        body:tr("gps_monitoring_info", context),
+      ),
+      Boarding(image:"assets/images/image2.png" ,
+        title:tr("sos_noti", context),
+        body:tr("sos_noti_info", context),
+      ),
+      Boarding(image:"assets/images/image3.png" ,
+        title:tr("addcontact", context),
+        body: tr("addcontact_info", context),
+      ),
+    ];
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        padding:  EdgeInsets.symmetric(horizontal: 25.0),
         child: Column(
           children: [
             Container(
@@ -77,26 +78,26 @@ class _Onborading_ScreenState extends State<Onborading_Screen> {
                           ),
                           Image.asset(
                               "${boarding[index].image}"),
-                          const SizedBox(
+                           SizedBox(
                             height: 40,
                           ),
                           Text(
-                            "${boarding[index].title} ",style:const TextStyle(
+                            "${boarding[index].title} ",style: TextStyle(
                               fontWeight: FontWeight.w700,
                               fontSize: 20,
                               color: Color(0xff5095FF)
                           ),
                           ),
-                          const  SizedBox(
+                            SizedBox(
                             height: 15,
                           ),
                           Text(
-                            "${boarding[index].body}",style:const TextStyle(
+                            "${boarding[index].body}",style: TextStyle(
                             fontWeight: FontWeight.w400,
                             fontSize: 16,color: Color(0xffA8A29E),
                           ),
                           ),
-                          const SizedBox(
+                           SizedBox(
                             height: 20,
                           ),
                         ],
@@ -106,7 +107,7 @@ class _Onborading_ScreenState extends State<Onborading_Screen> {
             ),
             SmoothPageIndicator(
               controller: controller,
-              effect:const ExpandingDotsEffect(
+              effect: ExpandingDotsEffect(
                   dotHeight: 10,
                   dotWidth: 10,
                   spacing: 5,
@@ -114,7 +115,7 @@ class _Onborading_ScreenState extends State<Onborading_Screen> {
               ),
               count: boarding.length,
             ),
-            const  SizedBox(
+              SizedBox(
               height: 32,
             ),
             Container(
@@ -131,11 +132,11 @@ class _Onborading_ScreenState extends State<Onborading_Screen> {
                       builder:(context) => Login_Screen(), ));
                   }else{
                     controller.nextPage(
-                        duration:const Duration(seconds: 1),
+                        duration: Duration(seconds: 1),
                         curve:Curves.bounceInOut);
                   }
                   },
-                child:const Text( "Next",style: TextStyle(
+                child: Text(tr("next", context),style: TextStyle(
                     fontSize: 15,fontWeight: FontWeight.w500,color: Colors.white
                 ),),
               ),
